6f6bf4d6eae389c3591b8f6c62ac728fd4f674ff
sujay pradhan <maxff@sujays-Mac-mini.local>
2025-07-27 20:10:43 +0530

    initialize the shadcdn

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { Button } from "./components/ui/button"
 
 
 function App() {
@@ -5,6 +6,7 @@ function App() {
   return (
     <>
     <h1>hello world</h1>
+    <Button>Click me</Button>
     </>
   )
 }
